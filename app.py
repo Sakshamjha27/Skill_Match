9fe7f98e507ca533dfc66ae05641c42073593608
Sakshamjha27 <sakshamjha279@gmail.com>
2026-01-20 12:09:21 +0530

    push 3

--- app.py
+++ app.py
@@ -33,29 +33,30 @@ st.write(tips)
 
 job_desc =  st.text_area(':red[Copy and paste your Job Descriptions over here:]',max_chars=5000)
 if st.button(':red[SUBMIT]'):
-    prompt = f'''
-    <Role> Your are an expert in analyzing the resume and matching with job description and its been 5 years your doing this .Hence your are an expreinced expert in resume analyazing .
-    <Goal> Match the resume and the job desciptions provided by the applicant and create a detailed report about the Resume .  
-    <Context> The following content has been provided by the applicant .
-    * Resume : {file_text}
-    * Job Description : {job_desc}
-    <Format> The report should follow these steps:
-    * Give a breif description of applicant in 3 to 5 lines about the resume .
-    * Describe the percentage of what a chances will the resume wil be shortlisted . 
-    * Need not be the exact percentage , you may give the interval of percetage . 
-    * Give the expected ATS score with matching and non-matching key words . 
-    * Perfrom SWOT Analysis and explain each parameter ie Strength,Weakness,Oppurnity,thread . 
-    * Give what al section can be changed in order to improve the ATS score and section percentage . 
-    * Show both current version and improve version of the sections in the resume . 
-    * Create 2 sample resume  which can maximaize the ATS score and section percentange.
-    <Instruction> 
-    * Use Bullets points for explation , wherever possible, 
-    * Create tables for description where ever required . 
-    * Do not add any new skill in the sample resumes .
-    * The format of sample resume shoud be ina such a way that can be copied and pasted directly in word  .
-    '''
+    with st.spinner('Processing...') :
+        prompt = f'''
+        <Role> Your are an expert in analyzing the resume and matching with job description and its been 5 years your doing this .Hence your are an expreinced expert in resume analyazing .
+        <Goal> Match the resume and the job desciptions provided by the applicant and create a detailed report about the Resume .  
+        <Context> The following content has been provided by the applicant .
+        * Resume : {file_text}
+        * Job Description : {job_desc}
+        <Format> The report should follow these steps:
+        * Give a breif description of applicant in 3 to 5 lines about the resume .
+        * Describe the percentage of what a chances will the resume wil be shortlisted . 
+        * Need not be the exact percentage , you may give the interval of percetage . 
+        *   Give the expected ATS score with matching and non-matching key words . 
+        * Perfrom SWOT Analysis and explain each parameter ie Strength,Weakness,Oppurnity,thread . 
+        * Give what al section can be changed in order to improve the ATS score and section percentage . 
+        * Show both current version and improve version of the sections in the resume . 
+        * Create 2 sample resume  which can maximaize the ATS score and section percentange.
+        <Instruction> 
+        * Use Bullets points for explation , wherever possible, 
+        * Create tables for description where ever required . 
+        * Do not add any new skill in the sample resumes .
+        * The format of sample resume shoud be ina such a way that can be copied and pasted directly in word  .
+        '''
 
-    response = model.invoke(prompt)
-    st.write(response.content) 
+        response = model.invoke(prompt)
+        st.write(response.content) 
     
     
